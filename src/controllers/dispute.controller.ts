49608867623d79dc39922490f60e74db7ee21cd4
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-13 21:33:49 +0545

    test dispute

--- src/controllers/dispute.controller.ts
+++ src/controllers/dispute.controller.ts
@@ -6,7 +6,7 @@ import {
   resolveDisputeSchema,
 } from "../validators/dispute.validator";
 
-export const disputeController = { //dispute controller handles the dispute related requests
+export const disputeController = { 
   async create(req: Request, res: Response) {
     const payload = createDisputeSchema.parse(req.body);
     const dispute = await disputeService.createDispute(payload, req.user!);
