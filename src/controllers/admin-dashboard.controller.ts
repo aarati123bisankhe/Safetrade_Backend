a225a4e6692eb87a8d1ec64659205f58739965bd
Aarati Thapa <aaratichettri46@gmail.com>
2026-07-13 23:22:08 +0545

    handling admin dashboard requests

--- src/controllers/admin-dashboard.controller.ts
+++ src/controllers/admin-dashboard.controller.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 import { adminDashboardService } from "../services/admin-dashboard.service";
 import { dashboardQuerySchema } from "../validators/admin-dashboard.validator";
 
-export const adminDashboardController = {
+export const adminDashboardController = { // Controller for handling admin dashboard requests
   async getDashboard(req: Request, res: Response) {
     const query = dashboardQuerySchema.parse(req.query);
     const dashboard = await adminDashboardService.getDashboard(query, req.user!);
